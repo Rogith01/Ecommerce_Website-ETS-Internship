daef52f10248e9b67268d5047d76bef3920e7bc0
Rogith01 <akrogith777@gmail.com>
2024-07-07 21:40:16 +0530

    initial commit

---
+++ menu.js
@@ -0,0 +1,7 @@
+
+const mobileMenu = document.getElementById('mobile-menu');
+const navLinks = document.querySelector('.nav-links');
+
+mobileMenu.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
---
+++ music.js
@@ -0,0 +1,31 @@
+function playMusic1(){
+    var music = new Audio('click.mp3');
+    music.play();
+}
+
+
+function playMusic2(){
+    var music = new Audio('click 2.wav');
+    music.play();
+}
+
+
+function playMusic3(){
+    var music = new Audio('click 3.wav');
+    music.play();
+}
+
+function playMusic(){
+    var music = new Audio('click 1.wav');
+    music.play();
+}
+
+function playMusic4(){
+    var music = new Audio('click 4.wav');
+    music.play();
+}
+
+function playMusic5(){
+    var music = new Audio('click 5.mp3');
+    music.play();
+}
